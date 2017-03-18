cc6f3eb8a08180f1197f0b1936fafaa1035c8c4c
Bence Kaulics <Csatacsibe@users.noreply.github.com>
2017-03-18 15:14:41 +0100

    Update Si7021_cli.h

--- test/cli/inc/Si7021_cli.h
+++ test/cli/inc/Si7021_cli.h
@@ -1,1 +1,11 @@
+#ifndef SI7021_CLI_H_
+#define SI7021_CLI_H_
 
+#include "stm32f4xx_hal.h"
+
+typedef uint8_t (*print_t)(uint8_t* buf, uint16_t len);
+
+void Si7021_cli_init(print_t func);
+void Si7021_cli_engine(uint8_t* char_in);
+
+#endif /* SI7021_CLI_H_ */
